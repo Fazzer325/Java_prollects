76ae66c208c2d09e59f403296ad5f5b90cbba17f
cris-pc <cristoramos16@hotmail.com>
2024-10-26 08:01:18 -0600

    update

---
+++ Main.java
@@ -0,0 +1,6 @@
+import static Mods.Test.*;
+public class Main {
+    public static void main(String[] args) {
+        Saludar("Raul");
+    }
+}
---
+++ Mods/Test.java
@@ -0,0 +1,7 @@
+package Mods;
+
+public class Test {
+    public static void Saludar(String nombre) {
+        System.out.println("Hola Bienvenido " + nombre);
+    }
+}
