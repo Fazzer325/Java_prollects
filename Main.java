b42f882415ab12a3efb838439cddb35f95d6d9ed
cris-pc <cristoramos16@hotmail.com>
2024-10-26 21:02:08 -0600

    update

--- Main.java
+++ Main.java
@@ -5,6 +5,5 @@ public class Main {
                 "Carlos","Raul","Manolo","Sara","Maria","Karla"
         };
         for (String nombre : nombres) Saludar(nombre);
-        //https://code-with-me.global.jetbrains.com/_awwXkXCg2AZFTItUVwqGQ#p=IU&fp=D80AE069FBF9F85C71C52BC13A6D01E93A338F51C8D12F679FC05DE8AF52C6E4&newUi=true
     }
 }
